e7907aff09308dd64e8fd11e378a0334315781b3
edutbx <141964723+edutbx@users.noreply.github.com>
2025-03-10 21:46:34 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,19 @@
+function abrirProjeto1(){
+    window.open("https://pedro-albertini.github.io/Projeto-Integrador");
+};
+
+function abrirProjeto2(){
+    window.open("https://pedro-albertini.github.io/SiteGoku/");
+};
+
+function abrirProjeto3(){
+    window.open("https://edutbx.github.io/calculadora-Js/");
+};
+
+function abrirGithub(){
+    window.open("https://github.com/edutbx");
+};
+
+function abrirLinkedin(){
+    window.open("https://www.linkedin.com/in/eduardo-tebexerini-4b7a102ba/");
+};
